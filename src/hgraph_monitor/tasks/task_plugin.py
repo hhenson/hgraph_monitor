3e0cdc8093f7104168da7f43be5075b7a5998e5b
Howard Henson <howard@henson.me.uk>
2024-08-27 22:23:44 +0400

    Get to the point of breaking

--- src/hgraph_monitor/tasks/task_plugin.py
+++ src/hgraph_monitor/tasks/task_plugin.py
@@ -1,7 +1,10 @@
-from hgraph import graph
-from hgraph_monitor.tasks.task_api import task_starting
+from hgraph import graph, const, TSB, TSD
+from hgraph_monitor.tasks.task_api import task_starting, TaskStatus, TaskStatusEnum
+
+from frozendict import frozendict as fd
 
 
 @graph
 def register_task_plugin():
-    ...
+    states = const(fd({'a': fd(status=TaskStatusEnum.STARTED, reason=''),}), TSD[str, TSB[TaskStatus]])
+    task_starting(states=states)
